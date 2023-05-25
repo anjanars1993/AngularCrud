f5d5b1c63234905c980579822c57b0b325a9d04b
Anjana <anjanars1993@gnail.com>
2023-05-25 08:49:36 +0530

    commit #2

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model';
 
 @Component({
   selector: 'app-create-employee',
@@ -7,13 +8,21 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent {
+departments:Department[]=[
+  {id:1,name:"Help Desk"},
+  {id:2,name:"Service"},
+  {id:3,name:"Sales"},
+  {id:4,name:"Marketing"},
+
+]
+
   fullName:string="";
   email:string="";
   gender:string="female";
   phoneNumber:string="";
   contactPreference:string="";
   isActive:boolean=false;
-  department:string="";
+  department:string="3";
   saveEmployee(empForm:NgForm):void{
     console.log(empForm);
     console.log(empForm.value);
---
+++ src/app/models/department.model.ts
@@ -0,0 +1,11 @@
+export class Department
+{
+    id:number;
+    name:string;
+
+    constructor(){
+        this.id=1;
+        this.name="Help Desk"
+
+    }
+}
